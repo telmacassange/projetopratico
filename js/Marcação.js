df544ed6e960cc09bad8a7b519c9e625b87483b5
telmacassange <telmacassange1996rgmal.com>
2022-05-22 21:24:43 +0100

    alteração do estilo

---
+++ js/Marcação.js
@@ -0,0 +1,20 @@
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let elementH2 = document.getElementById("text-resultado");
+
+//Usando funções
+
+function marcacao(){
+
+    //concatenar é juntar textos
+
+    let tex1t= parseFloat(input1.value);
+    let text2=  parseFloat(input2.value);
+    let resultado = text1 +text2;
+
+
+    elementH2.innerHTML = "O resultado foi: " + obrigada;
+
+    input1.value = "entraremos em contacto em breve";
+    input2.value = "entraremos em contacto em breve";
+}
